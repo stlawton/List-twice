fa80d10ac79a8c2581ea8e7072fd46a4eb6e575a
stlawton <lawtons347@gmail.com>
2025-08-13 18:59:11 -0400

    Update list_twice.py

--- list_twice.py
+++ list_twice.py
@@ -3,8 +3,8 @@ input_list = [] # Initialize list to track input order
 ordered_list=[] # Initialize sorted list
 index = 0 
 while item != 0:  # Run loop until 0 is input, loop adds each input to the input_list and then sorts that list, print both
-  input_list[index] = item
-  ordered_list = input_list.sort() 
+  input_list.append(item)
+  ordered_list = sorted(input_list) 
   print(f"The list now: {input_list}")
   print(f"The list in order: {ordered_list}")
   item = int(input("New item: ")) # Repeat input prompt until user enters 0
